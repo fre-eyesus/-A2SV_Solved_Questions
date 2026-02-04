776e810de842f88a2f9281ff8ed828f70f1d6dab
Fireweld Tesfaye <134558351+fre-eyesus@users.noreply.github.com>
2026-02-04 15:29:53 +0300

    Implement split_and_join function in Python

---
+++ String-split-and-join.py
@@ -0,0 +1,12 @@
+
+
+def split_and_join(line):
+    
+    splited = line.split(" ")
+    joined= "-".join(splited)
+    return joined
+
+if __name__ == '__main__':
+    line = input()
+    result = split_and_join(line)
+    print(result)
