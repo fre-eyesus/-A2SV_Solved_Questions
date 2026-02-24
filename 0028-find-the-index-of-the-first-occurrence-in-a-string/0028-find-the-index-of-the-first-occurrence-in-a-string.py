346656f1dadd395b2716d8ac3816e7edc96f91dd
Fireweld Tesfaye <134558351+fre-eyesus@users.noreply.github.com>
2026-02-24 14:24:31 +0300

    Refactor strStr method for substring search
    Refactor strStr method to use sliding window approach for finding the first occurrence of a substring.

--- 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
+++ 0028-find-the-index-of-the-first-occurrence-in-a-string/0028-find-the-index-of-the-first-occurrence-in-a-string.py
@@ -1,7 +1,17 @@
 class Solution:
     def strStr(self, haystack: str, needle: str) -> int:
+        left = 0
+        right = len(needle)
+
+        while right < len(haystack)+1:
+            if haystack[left:right] == needle:
+                return left
+            left+= 1
+            right = left + len(needle)
+
+        return -1
         
-        if needle in haystack:
+        """if needle in haystack:
             return haystack.find(needle)
         else:
-            return -1
+            return -1 """
