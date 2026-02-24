d44a265b9f097bc1e91309aaaf1fad9043647cc0
Fireweld Tesfaye <134558351+fre-eyesus@users.noreply.github.com>
2026-02-24 14:03:59 +0300

    Time: 0 ms (100%), Space: 19.3 MB (76.71%) - LeetHub

---
+++ 0274-h-index/0274-h-index.py
@@ -0,0 +1,14 @@
+class Solution:
+    def hIndex(self, citations: List[int]) -> int:
+        citations.sort(reverse= True)
+      
+
+
+        count = 0
+        for i in range(len(citations)):
+            if citations[i] >= i+1:
+                count+= 1
+            
+   
+        
+        return count
