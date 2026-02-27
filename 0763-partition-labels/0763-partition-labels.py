06f9cf0908f200b263f809a4c35e1c30cf5a5854
Fireweld Tesfaye <134558351+fre-eyesus@users.noreply.github.com>
2026-02-27 15:23:50 +0300

    Time: 4 ms (43.96%), Space: 19.3 MB (79.09%) - LeetHub

---
+++ 0763-partition-labels/0763-partition-labels.py
@@ -0,0 +1,18 @@
+class Solution:
+    def partitionLabels(self, s: str) -> List[int]:
+
+        last_occurrence = defaultdict(str)
+        for i, char in enumerate(s):
+            last_occurrence[char] = i
+
+        partition_end = 0
+        partition_start = 0
+        partition_sizes = []
+
+        for i, char in enumerate(s):
+            partition_end = max(partition_end, last_occurrence[char])
+            if i == partition_end:
+                partition_sizes.append(i - partition_start + 1)
+                partition_start = i + 1
+
+        return partition_sizes
