5a478560ad626447be5fcd1aeb25913a0df4ad1f
Fireweld Tesfaye <134558351+fre-eyesus@users.noreply.github.com>
2026-02-28 23:20:03 +0300

    Time: 99 ms (87.33%), Space: 22.1 MB (60.33%) - LeetHub

--- 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
+++ 0992-subarrays-with-k-different-integers/0992-subarrays-with-k-different-integers.py
@@ -1,21 +1,35 @@
 class Solution:
     def subarraysWithKDistinct(self, nums: List[int], k: int) -> int:
-        def atMost(k):
-            count = Counter()
-            l = 0
-            total = 0
 
-            for r in range(len(nums)):
-                count[nums[r]] += 1
+        count=0
+        
+        l_near,l_far,right = 0  , 0, 0
+        
+        dis = defaultdict(int)
 
-                while len(count) > k:
-                    count[nums[l]] -= 1
-                    if count[nums[l]] == 0:
-                        del count[nums[l]]
-                    l += 1
+        for r in range(len(nums)):
+            dis[nums[r]] += 1
 
-                total += r - l + 1
+            while len(dis) > k:
+                dis[nums[l_near]] -= 1
 
-            return total
+                if dis[nums[l_near]] == 0:
+                    dis.pop(nums[l_near])
 
-        return atMost(k) - atMost(k - 1)
+                l_near += 1
+                l_far = l_near
+            while dis[nums[l_near]] > 1:
+                dis[nums[l_near]] -= 1
+                l_near += 1
+            
+            if len(dis) == k:
+                count += l_near - l_far + 1
+            
+    
+        
+        return count
+
+
+
+
+        
