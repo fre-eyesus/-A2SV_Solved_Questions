fd42eb34d11ae7e945ad2d5c5acf3f1b5345ca0f
Fireweld Tesfaye <134558351+fre-eyesus@users.noreply.github.com>
2026-03-04 12:15:08 +0300

    Add solution for Range Sum Query 2D - Immutable

--- leetcode/0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
+++ leetcode/0304-range-sum-query-2d-immutable/0304-range-sum-query-2d-immutable.py
@@ -1,27 +1,27 @@
 class NumMatrix:
 
-    def __init__(self, matrix: List[List[int]]):
-        
+    def __init__(self, matrix):
+     
         n = len(matrix)
         m = len(matrix[0])
 
-        self.pre = [[0 for i in range(m+1)] for j in  range(n+1)]
+        self.pre = [[0] * (m + 1) for _ in range(n + 1)]
 
-        for i in range(n):
-            for j in range(m):
-                self.pre[i][j] = matrix[i][j] + self.pre[i - 1][j] + self.pre[i][j-1] - self.pre[i-1][j-1]
+        for r in range(n):
+            pref = 0
+            for c in range(m):
+                pref += matrix[r][c]
+                above = self.pre[r][c+1]
 
+                self.pre[r+1][c+1] = pref + above
 
-    def sumRegion(self, row1: int, col1: int, row2: int, col2: int) -> int:
-        box = self.pre[row2][col2]
-        l_box = self.pre[row2][col1-1]
-        u_box = self.pre[row1-1][col2]
+               
+    def sumRegion(self, row1, col1, row2, col2):
+        row1,col1,row2,col2 = row1+1,col1+1,row2+1,col2+1
 
-        overlap = self.pre[row1-1][col1-1]
+        bottomR = self.pre[row2][col2]
+        above = self.pre[row1-1][col2]
+        left = self.pre[row2][col1-1]
+        topLeft = self.pre[row1-1][col1-1]
 
-        return box - l_box - u_box + overlap
-
-
-# Your NumMatrix object will be instantiated and called as such:
-# obj = NumMatrix(matrix)
-# param_1 = obj.sumRegion(row1,col1,row2,col2)
+        return bottomR - above - left + topLeft
