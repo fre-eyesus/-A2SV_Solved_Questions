918054dad87f1d39e628433cdcfb7a3cc124f5bc
Fireweld Tesfaye <134558351+fre-eyesus@users.noreply.github.com>
2026-04-27 18:24:28 +0300

    Time: 0 ms (100%), Space: 19.3 MB (72.29%) - LeetHub

--- 0965-univalued-binary-tree/0965-univalued-binary-tree.py
+++ 0965-univalued-binary-tree/0965-univalued-binary-tree.py
@@ -7,16 +7,30 @@
 class Solution:
     def isUnivalTree(self, root: Optional[TreeNode]) -> bool:
         num = root.val
-        def dfs(root):
-            if not root:
-                return True 
-            if root.val != num:
-                return False
+        # def dfs(root):
+        #     if not root:
+        #         return True 
+        #     if root.val != num:
+        #         return False
             
         
-            return  dfs(root.left) and dfs(root.right)
+        #     return  dfs(root.left) and dfs(root.right)
 
-        return dfs(root)
+        # return dfs(root)
 
+        q = deque([root])
+        while q:
+
+            node = q.popleft()
+
+            if node.val != num:
+                return False
+            
+            if node.left:
+                q.append(node.left)
+            if node.right:
+                q.append(node.right)
+            
+        return True
 
     
